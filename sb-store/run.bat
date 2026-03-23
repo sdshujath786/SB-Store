6182775ca8748daf8f76357992b36a3c3e11ff4e
sdshujath786 <sdshujath786@gmail.com>
2026-03-23 17:25:14 +0530

    removed localhost (fix 502)

--- sb-store/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting SB Store Server...
-echo.
-echo Server will run at: http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-python -m http.server 8000
-pause
